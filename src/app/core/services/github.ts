761dcbeb03c770623fa9ed5d4158913f76c67c8a
Goutham E <goutham.e@simplify3x.com>
2026-07-29 20:03:49 +0530

    feat: add dynamic authorization headers to GitHub service API calls

--- src/app/core/services/github.ts
+++ src/app/core/services/github.ts
@@ -1,16 +1,26 @@
 import { Service, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Service()
 export class Github {
     private http = inject(HttpClient); 
     private readonly baseUrl = 'https://api.github.com/users';
+    private readonly token = 'xxxxxx';
+    private useAuth:Boolean = false;
+    
+    private get getHeaders(): HttpHeaders{
+        let headers = new HttpHeaders()
+        if(this.useAuth){
+            headers = headers.set('Authorization', this.token);
+        }
+        return headers
+    }
 
     getUserData(username:String) {
-        return this.http.get(`${this.baseUrl}/${username}`)
+        return this.http.get(`${this.baseUrl}/${username}`, {headers : this.getHeaders})
     }
 
     getUseRepositories(username:String){
-        return this.http.get(`${this.baseUrl}/${username}/repos`)
+        return this.http.get(`${this.baseUrl}/${username}/repos`, {headers : this.getHeaders})
     }
 }
